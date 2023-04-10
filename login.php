c3518a7581d7fa54f444943081e88c269fea1b60
Aryan <91662750+aryans1204@users.noreply.github.com>
2023-04-10 19:06:19 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,9 +2,6 @@
 session_start();
 include "db_conn.php";
 
-$uname = $_POST['uname'];
-$pass = $_POST['password'];
-
 if(empty($uname)) {
     header("Location: index.php?error=User Name is required");
     exit();
