83ab27417e557e91ed17fe275bd5fa40625452e8
Aryan Sharma <arshar1204@gmail.com>
2023-04-15 11:56:33 +0800

    Added credit card schema

--- home.php
+++ home.php
@@ -1,6 +1,10 @@
 <?php
+include("db_conn.php");
 session_start();
-
+$usename = $_SESSION['user_name'];
+$ssql = "SELECT * FROM credit_card WHERE user_name LIKE '$usename'";
+$reesult = mysqli_query($conn, $ssql);
+$rowz = mysqli_fetch_assoc($reesult);
 if(isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
     ?>
 
@@ -11,8 +15,8 @@ if(isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
             <link rel="styelseheet" type="text/css" href="styles.css">
         </head>
         <body>
-            <h1>Hello, <?php echo $_SESSION['user_name']; ?></h1>
-            <h2>Your credit card number is 4000 0000 0000 0000 </h2>
+            <h1>Hello, <?php echo $rowz['user_name']; ?></h1>
+            <h2>Your credit card number is <?php echo $rowz['cNum'] ?></h2>
             <a href="logout.php">Logout</a>
         </body>
     </html>
